af52c1746b3298b4ba29311e4946cdedf85e9d18
Javier Callejón <javicallejon88@gmail.com>
2023-04-16 15:38:00 +0200

    Clean repository + css and html hard changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -68,26 +68,4 @@ jQuery(function ($) {
         .addClass("active");
     }
   });
-
-  /* ----------------------------------------------------------- */
-  /*  2. BUTTON SMOOTH SCROLL ( VIEW MY WORK )
-  /* ----------------------------------------------------------- */
-
-  $(".mu-reservation-btn").on("click", function (e) {
-    e.preventDefault();
-    var target = this.hash,
-      $target = $(target);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top,
-        },
-        1000,
-        "swing",
-        function () {
-          window.location.hash = target;
-        }
-      );
-  });
 });
