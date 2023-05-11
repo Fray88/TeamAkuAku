2a85d56def4bc31d49147c0d82c5e518fa2c1a00
Javier Callejón <javicallejon88@gmail.com>
2023-05-11 19:09:12 +0200

    Finish website

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const burguerButton = document.querySelector("#burguer-button");
 const nav = document.querySelector("#main-nav");
+const navButton = document.querySelectorAll(".main-nav-list-item a");
 
 const topLineBurguer = document.querySelector(".top-line");
 const middleLineBurguer = document.querySelector(".middle-line");
@@ -11,4 +12,11 @@ burguerButton.addEventListener("click", () => {
   topLineBurguer.classList.toggle("active-top-line");
   middleLineBurguer.classList.toggle("active-middle-line");
   bottomLineBurguer.classList.toggle("active-bottom-line");
+});
+
+navButton.forEach((button) => {
+  console.log(button)
+  button.addEventListener("click", () => {
+    nav.classList.toggle("activo");
+  });
 });
